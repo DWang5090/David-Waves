82b0dfb2f3f98c81c64682b6e5e20606d76dfbd1
DWang5090 <davidcxwang@hotmail.com>
2016-01-25 20:41:52 -1000

    First commit for David_Waves

---
+++ Assets/Wave_Motion.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave_Motion : MonoBehaviour {
+
+	private Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = startPos + new Vector3(0.0f, Mathf.Sin(Time.time * 1.5f) * 3.0f, 0.0f);
+	}
+}
