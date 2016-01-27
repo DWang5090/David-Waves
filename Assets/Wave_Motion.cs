2947eae04aa7fc57a31b4a53a97a121664134e1f
Alberto Gonzalez <whatwehaveunlearned@gmail.com>
2016-01-27 12:26:29 -1000

    Second Commit to David-Waves

--- Assets/Wave_Motion.cs
+++ Assets/Wave_Motion.cs
@@ -12,5 +12,7 @@ public class Wave_Motion : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = startPos + new Vector3(0.0f, Mathf.Sin(Time.time * 1.5f) * 3.0f, 0.0f);
+		Vector2 v2 = new Vector2 (-4.5f, 0.0f) + (new Vector2 (12.0f, 8.0f) * 1.0f/60.0f);
+		Debug.Log (v2);
 	}
 }
