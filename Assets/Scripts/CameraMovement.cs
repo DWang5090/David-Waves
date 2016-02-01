aecc55b24dcf814c84ae8c397a4a247435273dad
David Wang <David Wang>
2016-02-01 01:42:48 -1000

    Waves Final

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float speed = 1.0f;
+
+	private Vector3 lastMouse = new Vector3 (255,255,255);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 dir = new Vector3 ();
+
+		if (Input.GetKey (KeyCode.W)) {
+			dir.y += 0.1f;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			dir.y -= 0.1f;
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			dir.x -= 0.1f;
+		}
+
+		if (Input.GetKey (KeyCode.D)) {
+			dir.x += 0.1f;
+		}
+
+
+		if (Input.GetKey (KeyCode.UpArrow)){ 
+			dir.z += 0.1f;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)){ 
+			dir.z -= 0.1f;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)){ 
+			Vector3 rotate_cam = new Vector3 (0, Input.GetAxis ("Horizontal"), 0);
+			transform.Rotate (rotate_cam * speed * Time.deltaTime); 
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)){ 
+			Vector3 rotate_cam = new Vector3 (0, Input.GetAxis ("Horizontal"), 0);
+			transform.Rotate (rotate_cam * speed * Time.deltaTime); 
+		}
+
+		transform.Translate (dir * speed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Wave_Motion.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wave_Motion : MonoBehaviour {
+
+	private Vector3 startPos;
+	float largeRange = 10.0f;
+	float smallRange = 3.0f;
+	float timer = 0.0f;
+	float currRange = 3.0f;
+	bool changeRange = true;
+	float newRange = 1.0f;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timer += (Time.deltaTime * 1.0f) % (2.0f + Mathf.PI);
+		transform.position = startPos + new Vector3 (0.0f, Mathf.Sin (timer * 1.5f) * currRange, 0.0f);
+		if (changeRange) {
+			currRange = Mathf.MoveTowards (currRange, largeRange, Time.deltaTime + newRange);
+			Debug.Log (changeRange);
+		} else {
+			currRange = Mathf.MoveTowards (currRange, smallRange, Time.deltaTime + newRange);
+			Debug.Log (changeRange);
+		}
+
+		if (Input.GetKey (KeyCode.T)) {
+			changeRange = !changeRange;
+		}
+	}
+		
+}
